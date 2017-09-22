0d90338ad2aa487f984c4618eec48ae270791881
Josh Petras <joshpetras@gmail.com>
2017-09-22 17:00:09 +0200

    Add Input Functionality - project steps 5 to 7.

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,8 +1,18 @@
+$(document).ready(function(){
+  $("#search-btn").click(function(){
+    Trackster.searchTracksByTitle($("#search-txt").val());
+  });
+
+});
+
+
+
+
 var Trackster = {};
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
+  Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
 
